540f134fa97e6499ae5c53ad14318f026444411b
rlatngus <31719758+rlatngus@users.noreply.github.com>
2018-09-07 18:12:29 +0900

    02

---
+++ 02/app.js
@@ -0,0 +1,24 @@
+var str = 'hello world!';
+console.log(str);
+
+//1~100까지의 숫자 출력
+for(var i =0; i<=100; i++){
+    console.log(i);
+}
+
+//1~100까지의 숫자중 홀수 출력
+for(var i =0; i<=100; i++){
+    if(i%2){
+        console.log(i);
+    }
+}
+
+//구구단
+for(var i=1; i<10; i++){
+    console.log(i+'단');
+    for(var j=1; j<10; j++){
+        console.log(i+'x'+k+'='+(i*k));
+    }
+}
+
+
