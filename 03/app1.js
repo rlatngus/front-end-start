766bca6e0bc684110a11fd49ee2a9ea23f3bdc61
rlatngus <31719758+rlatngus@users.noreply.github.com>
2018-09-14 17:50:03 +0900

    03

---
+++ 03/app1.js
@@ -0,0 +1,5 @@
+function test() {
+    console.log("test()");
+}
+
+var id = setTimeout(test,100);
---
+++ 03/app2.js
@@ -0,0 +1,7 @@
+var dom = document.getElementById("debug")
+
+//console.log(dom);
+
+var dom2= document.querySelectorAll("#debug");
+
+console.log(dom2);
